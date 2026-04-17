66518668e42fc8d4dd44e5dc776feebf64e924c3
Pedro Teles <telles021205@gmail.com>
2026-04-16 21:02:34 -0300

    Início da implementação de sessões 01

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 
-# Framework: Flask / Jinja 2
 app = Flask(__name__, template_folder='app/template', static_folder='app/static')
 
-# CONFIGURAÇÃO DO BANCO DE DADOS 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# Config flask
+app.config['SECRET_KEY'] = 'uma_chave_secreta_muito_louca_e_segura_profinder'
 
+# Config bd  
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'profinder.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Extensões
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Por favor, faça login para acessar esta página."
+login_manager.login_message_category = "info"
 
-class Usuario(db.Model):
+
+class Usuario(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     cpf = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(128), nullable=False)
+    tipo_conta = db.Column(db.String(20), default='cliente')
 
-    def __repr__(self):
-        return f'<Usuario {self.nome}>'
-
-class Profissional(db.Model):
+class Profissional(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     cpf = db.Column(db.String(11), unique=True, nullable=False) 
@@ -32,14 +40,21 @@ class Profissional(db.Model):
     profissao = db.Column(db.String(50), nullable=False)
     bairro = db.Column(db.String(50), nullable=False)
     avaliacao = db.Column(db.Float, default=5.0) 
-    foto = db.Column(db.String(255), nullable=False) 
-    def __repr__(self):
-        return f'<Profissional {self.nome} - {self.profissao}>'
+    foto = db.Column(db.String(255), default='https://cdn-icons-png.flaticon.com/512/3135/3135715.png')
+    tipo_conta = db.Column(db.String(20), default='profissional')
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = Usuario.query.get(int(user_id))
+    if user:
+        return user
+    return Profissional.query.get(int(user_id))
 
 with app.app_context():
     db.create_all()
 
-# ROTAS DAS PÁGINAS 
+# Rotas das páginas 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -47,49 +62,14 @@ def index():
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
- 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        print(f"--- NOVA TENTATIVA DE LOGIN ---")
-        print(f"Email recebido: {email}")
-        #Verificação de login via BD
-        
-    return render_template('login.html')
-
-# Cadastro de Cliente Padrão
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro_cliente():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        email = request.form.get('email')
-        print(f"--- NOVO CADASTRO DE CLIENTE: {nome} | {email} ---")
-        # Futuramente: Salvar no Banco de Dados
-        
-    return render_template('cadastro.html')
-
-# Cadastro de Profissional
-@app.route('/cadastro-profissional', methods=['GET', 'POST'])
-def cadastro_profissional():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        profissao = request.form.get('profissao')
-        print(f"--- NOVO CADASTRO DE PROFISSIONAL: {nome} | {profissao} ---")
-        # Futuramente: Salvar no Banco de Dados
-        
-    return render_template('cadastroProfissional.html')
 
 @app.route('/ajuda')
 def ajuda():
     return render_template('ajuda.html')
 
-
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
-
 
 @app.route('/busca-servicos')
 def busca_servicos():
@@ -98,8 +78,116 @@ def busca_servicos():
 @app.route('/busca-profissionais/<categoria>')
 def busca_profissionais(categoria):
     profissionais_encontrados = Profissional.query.filter_by(profissao=categoria).all()
-    
     return render_template('busca_profissionais.html', profissionais=profissionais_encontrados, categoria=categoria)
+
+# Rotas de sessões
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro_cliente():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        cpf_raw = request.form.get('cpf') # Pegamos o CPF com a máscara (pontos e traços)
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        confirma_senha = request.form.get('confirma_senha')
+
+        # Limpa os pontos e traços da máscara do HTML antes de validar
+        cpf = cpf_raw.replace('.', '').replace('-', '') if cpf_raw else None
+
+        if len(cpf) != 11:
+            flash("O CPF deve conter exatamente 11 dígitos!", "danger")
+            return render_template('cadastro.html', nome=nome, cpf=cpf_raw, email=email)
+
+        if senha != confirma_senha:
+            flash("As senhas não coincidem. Tente novamente!", "danger")
+            return render_template('cadastro.html', nome=nome, cpf=cpf_raw, email=email)
+
+        email_existe = Usuario.query.filter_by(email=email).first() or Profissional.query.filter_by(email=email).first()
+        cpf_existe = Usuario.query.filter_by(cpf=cpf).first() or Profissional.query.filter_by(cpf=cpf).first()
+
+        if email_existe or cpf_existe:
+            flash("Este E-mail ou CPF já estão cadastrados no sistema.", "danger")
+            return render_template('cadastro.html', nome=nome, cpf=cpf_raw, email=email)
+
+        senha_criptografada = bcrypt.generate_password_hash(senha).decode('utf-8')
+        novo_usuario = Usuario(nome=nome, cpf=cpf, email=email, senha=senha_criptografada)
+        
+        db.session.add(novo_usuario)
+        db.session.commit()
+        
+        login_user(novo_usuario)
+        return redirect(url_for('index'))
+        
+    return render_template('cadastro.html')
+
+@app.route('/cadastro-profissional', methods=['GET', 'POST'])
+def cadastro_profissional():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        cpf_raw = request.form.get('cpf')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        confirma_senha = request.form.get('confirma_senha')
+        profissao = request.form.get('profissao')
+        regiao = request.form.get('regiao')
+        descricao = request.form.get('descricao')
+
+        cpf = cpf_raw.replace('.', '').replace('-', '') if cpf_raw else None
+
+        if len(cpf) != 11:
+            flash("O CPF deve conter exatamente 11 dígitos!", "danger")
+            return render_template('cadastroProfissional.html', nome=nome, cpf=cpf_raw, email=email, profissao=profissao, regiao=regiao, descricao=descricao)
+
+        if senha != confirma_senha:
+            flash("As senhas não coincidem. Tente novamente!", "danger")
+            return render_template('cadastroProfissional.html', nome=nome, cpf=cpf_raw, email=email, profissao=profissao, regiao=regiao, descricao=descricao)
+
+        email_existe = Usuario.query.filter_by(email=email).first() or Profissional.query.filter_by(email=email).first()
+        cpf_existe = Usuario.query.filter_by(cpf=cpf).first() or Profissional.query.filter_by(cpf=cpf).first()
+
+        if email_existe or cpf_existe:
+            flash("Este E-mail ou CPF já estão cadastrados no sistema.", "danger")
+            return render_template('cadastroProfissional.html', nome=nome, cpf=cpf_raw, email=email, profissao=profissao, regiao=regiao, descricao=descricao)
+
+        senha_criptografada = bcrypt.generate_password_hash(senha).decode('utf-8')
+
+        novo_profissional = Profissional(
+            nome=nome, cpf=cpf, email=email, senha=senha_criptografada, 
+            profissao=profissao, bairro=regiao
+        )
+        db.session.add(novo_profissional)
+        db.session.commit()
+
+        login_user(novo_profissional)
+        return redirect(url_for('index'))
+        
+    return render_template('cadastroProfissional.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        
+        usuario = Usuario.query.filter_by(email=email).first()
+        profissional = Profissional.query.filter_by(email=email).first()
+        user = usuario or profissional
+
+        # Se a pessoa existir e a senha criptografada for igual à digitada
+        if user and bcrypt.check_password_hash(user.senha, senha):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash("E-mail ou senha incorretos. Verifique os seus dados.", "danger")
+            return redirect(url_for('login'))
+        
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
